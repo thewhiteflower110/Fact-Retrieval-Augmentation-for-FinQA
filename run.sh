9dafa743a49c7cb0b06210cf7fb807f9a141bac4
Juhi Kamdar <43910571+thewhiteflower110@users.noreply.github.com>
2022-10-16 19:00:21 -0400

    Added code to run.sh
    Copied from assignment 2

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-
+python3 train.py --option pretrain --epochs 10 --lr 1e-3 --train data/cfimdb-train.txt --dev data/cfimdb-dev.txt --test data/cfimdb-test.txt --dev_out "cfimdb-dev-output.txt" --test_out "cfimdb-test-output.txt" --use_gpu
