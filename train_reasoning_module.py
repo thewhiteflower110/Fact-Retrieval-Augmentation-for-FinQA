30e1b86dc4c6c76edbef788b12cd620f63a7d75b
Juhi Kamdar <43910571+thewhiteflower110@users.noreply.github.com>
2022-10-26 14:58:47 -0400

    Update train_reasoning_module.py

--- train_reasoning_module.py
+++ train_reasoning_module.py
@@ -21,22 +21,21 @@ def seed_everything(seed=11711):
 
 def model_eval(dataloader, model, device):
     #model.eval() # switch to eval model, will turn off randomness like dropout
+    for step, batch in enumerate(tqdm(dataloader, desc=f'val-{epoch}', disable=TQDM_DISABLE)):
+        dic = pg_model.validation() 
+        #logging mechanism for loss
+
+    for step, batch in enumerate(tqdm(dataloader, desc=f'val-{epoch}', disable=TQDM_DISABLE)):
+        dic = span_model.validation()
+        #logging mechanism for loss
+        #outputs a dictionary.. Need to deal with it
     
     #try to use inbuilt test and predict functions
-    #Getting results on Fact Retriever Module:
-    for step, batch in enumerate(tqdm(val_dataloader, desc=f'val-{epoch}', disable=TQDM_DISABLE)):
-        dic = retriever.validation()
-        sents = batch["examples"]
-        #dic {"preds": preds, "labels": labels, "uids": uids}
-
-    for step, batch in enumerate(tqdm(val_dataloader, desc=f'val-{epoch}', disable=TQDM_DISABLE)):
-        batch_loss = questionClassificationModel.validation()
-      #logging mechanism for loss
-      #outputs a dictionary.. Need to deal with it
-    f1 = f1_score(dic["labels"], dic["preds"], average='macro')
+    
+    f1 = f1_score(batch["labels"], dic["preds"], average='macro')
     acc = accuracy_score(y_true, y_pred)
 
-    return acc, f1, dic["labels"], dic["labels"], sents
+    return acc, f1, dic["labels"], dic["labels"]
 
 def train(args):
     device = torch.device('cuda') if args.use_gpu else torch.device('cpu')
@@ -45,13 +44,18 @@ def train(args):
     pg_model = ProgramGeneration(config)
     pg_model = pg_model.to(device)
 
+    span_config = yaml.load(open(constants.span_config))
+    config = span_config["model"]["init_args"]
+    span_model = SpanSelection(config)
+    span_model = span_model.to(device)
+
     lr = args.lr
     ## specify the optimizer
     optimizer = AdamW(model.parameters(), lr=lr)
     best_dev_acc = 0
 
     #Note: make this parallel
-    #Finetune the Retriever Model
+    #Train the Program Generation Model
     for epoch in range(args.epochs):
         train_loss = 0
         num_batches = 0
@@ -66,19 +70,16 @@ def train(args):
         train_loss = 0
         num_batches = 0
         for step, batch in enumerate(tqdm(train_dataloader, desc=f'train-{epoch}', disable=TQDM_DISABLE)):
-            batch_loss = questionClassificationModel.train()
+            batch_loss = span_model.train()
             #logging mechanism for loss
             #update optimizer, backpropagation
 
+  def test():
+    z = model_eval(dataloader, model, device)
+    # logging mechanism for z
     #Getting results on Fact Retriever Module(Retriever + Question Classification):
     #evaluate/predict using test set
-    for step, batch in enumerate(tqdm(val_dataloader, desc=f'val-{epoch}', disable=TQDM_DISABLE)):
-            batch_loss = retriever.validation()
 
-    for step, batch in enumerate(tqdm(val_dataloader, desc=f'val-{epoch}', disable=TQDM_DISABLE)):
-            batch_loss = questionClassificationModel.validation()
-            #logging mechanism for loss
-            #outputs a dictionary.. Need to deal with it
 
 
 
