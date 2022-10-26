ccf1851ba92297bc91e887fc844c6fa181c914ed
Juhi Kamdar <43910571+thewhiteflower110@users.noreply.github.com>
2022-10-26 14:13:50 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 pwd Fact-Retrieval-Augmentation-for-FinQA/
-python setup.py install
-python3 train.py --option pretrain --epochs 10 --lr 1e-3 --train data/cfimdb-train.txt --dev data/cfimdb-dev.txt --test data/cfimdb-test.txt --dev_out "cfimdb-dev-output.txt" --test_out "cfimdb-test-output.txt" --use_gpu
+python3 setup.py install
+python3 train_fr_module.py --option pretrain --epochs 10 --lr 1e-3 --train data/cfimdb-train.txt --dev data/cfimdb-dev.txt --test data/cfimdb-test.txt --dev_out "cfimdb-dev-output.txt" --test_out "cfimdb-test-output.txt" --use_gpu
+python3 convert_fr_outputs.py
