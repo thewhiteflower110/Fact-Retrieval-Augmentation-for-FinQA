535da96601c70df225bf59e8f0f1e6b4974eac73
Juhi Kamdar <43910571+thewhiteflower110@users.noreply.github.com>
2022-10-26 16:40:09 -0400

    Added eval methods properly

--- train_fr_module.py
+++ train_fr_module.py
@@ -2,6 +2,7 @@ import time, random, numpy as np, argparse, sys, re, os
 #from types import SimpleNamespace
 import torch
 from torch import nn
+from torch.cuda import device_count
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from sklearn.metrics import classification_report, f1_score, recall_score, accuracy_score
@@ -25,24 +26,49 @@ def seed_everything(seed=11711):
     torch.backends.cudnn.deterministic = True
 
 # perform model evaluation in terms of the accuracy and f1 score.
-def model_eval(dataloader, model, device):
-    #model.eval() # switch to eval model, will turn off randomness like dropout
-    
+def retriever_model_eval(dataloader, retriever, device):
+    retriever.eval() # switch to eval model, will turn off randomness like dropout
+    final_loss = 0
+    criterion_loss = nn.CrossEntropyLoss(reduction='none', ignore_index=-1)
+    for step, batch in enumerate(tqdm(dataloader, desc=f'val-{epoch}', disable=TQDM_DISABLE)):            
+        input_ids = torch.tensor(batch["input_ids"]).to("cuda")
+        attention_mask = torch.tensor(batch["input_mask"]).to("cuda")
+        labels = torch.tensor(batch["labels"]).to("cuda")
+        output_dicts = retriever() #intuitively calling the forward method
+        logits = []
+        for output_dict in output_dicts:
+            logits.append(output_dict["logits"])
+        logits = torch.stack(logits)
+        loss = criterion_loss(logits.view(-1, logits.shape[-1]), labels.view(-1))
+        total_loss = loss.sum()
+        #Logging of Loss here
+        final_loss+=total_loss
+
+    avg_final_loss = final_loss/step
+    f1 = f1_score(output_dicts["labels"], output_dicts["preds"], average='macro')
+    acc = accuracy_score(output_dicts["labels"], output_dicts["preds"])
+
+    return acc, f1, output_dicts["labels"], output_dicts["preds"], avg_final_loss
+
+def qc_model_eval(dataloader, questionClassificationModel, device):
+    questionClassificationModel.eval()
     #try to use inbuilt test and predict functions
     #Getting results on Fact Retriever Module:
-    for step, batch in enumerate(tqdm(val_dataloader, desc=f'val-{epoch}', disable=TQDM_DISABLE)):
-        dic = retriever.validation()
-        sents = batch["examples"]
-        #dic {"preds": preds, "labels": labels, "uids": uids}
+    val_loss = 0
+    for step, batch in enumerate(tqdm(dataloader, desc=f'val-{epoch}', disable=TQDM_DISABLE)):
+        input_ids = torch.tensor(batch["input_ids"]).to("cuda")
+        attention_mask = torch.tensor(batch["input_mask"]).to("cuda")
+        labels = torch.tensor(batch["labels"]).to("cuda")
+        outputs = questionClassificationModel(input_ids,attention_mask,labels) #intuitively calling the forward method
+        loss = outputs.loss
+        total_loss+=loss
+        #Logging of Loss here
 
-    for step, batch in enumerate(tqdm(val_dataloader, desc=f'val-{epoch}', disable=TQDM_DISABLE)):
-        batch_loss = questionClassificationModel.validation()
-      #logging mechanism for loss
-      #outputs a dictionary.. Need to deal with it
-    f1 = f1_score(dic["labels"], dic["preds"], average='macro')
-    acc = accuracy_score(y_true, y_pred)
+    avg_total_loss = total_loss/step
+    f1 = f1_score(labels, outputs["preds"], average='macro')
+    acc = accuracy_score(labels, outputs["preds"])
 
-    return acc, f1, dic["labels"], dic["labels"], sents
+    return acc, f1, labels, outputs["preds"], avg_total_loss
 
 def save_model(model, optimizer, args, config, filepath):
     save_info = {
@@ -57,6 +83,9 @@ def save_model(model, optimizer, args, config, filepath):
 
     torch.save(save_info, filepath)
     print(f"save the model to {filepath}")
+
+def model_save():
+    pass
 
 def train(args):
     device = torch.device('cuda') if args.use_gpu else torch.device('cpu')
@@ -86,10 +115,10 @@ def train(args):
     questionClassificationModel = QuestionClassification(config)
     questionClassificationModel = questionClassificationModel.to(device)
 
-    lr = args.lr
+    #lr = args.lr
     ## specify the optimizer
-    optimizer = AdamW(model.parameters(), lr=lr)
-    best_dev_acc = 0
+    #optimizer = AdamW(model.parameters(), lr=lr)
+    #best_dev_acc = 0
 
     #Note: make this parallel
     #Finetune the Question Classification Model
@@ -110,6 +139,7 @@ def train(args):
             optimizer.step()
             #logging mechanism for loss
         #epoch wise loss logs here--
+        acc, f1, y_true, y_preds, avg_loss = qc_model_eval(val_dataloader, questionClassificationModel, device)
 
     #Note: make this parallel
     #Finetune the Question Classification Model
@@ -137,84 +167,32 @@ def train(args):
             #logging mechanism for loss
         #epoch wise loss logs here--
     
-    #Getting results on Fact Retriever Module(Retriever + Question Classification):
-    #evaluate/predict using test set
-    for step, batch in enumerate(tqdm(val_dataloader, desc=f'val-{epoch}', disable=TQDM_DISABLE)):
-            batch_loss = retriever.validation()
-
-    for step, batch in enumerate(tqdm(val_dataloader, desc=f'val-{epoch}', disable=TQDM_DISABLE)):
-            batch_loss = questionClassificationModel.validation()
-            #logging mechanism for loss
-            #outputs a dictionary.. Need to deal with it
-
-    '''
-    ## run for the specified number of epochs
-    for epoch in range(args.epochs):
-        model.train()
-        train_loss = 0
-        num_batches = 0
-        for step, batch in enumerate(tqdm(train_dataloader, desc=f'train-{epoch}', disable=TQDM_DISABLE)):
-            #Fact Retrieveing Module : Training stage.
-            logits = retriever(batch) #forward or train?
-            #get the loss and pass it to retirever to finetune it, as 
-            b_ids, b_type_ids, b_mask, b_labels, b_sents = batch[0]['token_ids'], batch[0]['token_type_ids'], batch[0][
-                'attention_mask'], batch[0]['labels'], batch[0]['sents']
-
-            b_ids = b_ids.to(device)
-            b_mask = b_mask.to(device)
-            b_labels = b_labels.to(device)
-
-            optimizer.zero_grad()
-            logits = model(b_ids, b_mask)
-            loss = F.nll_loss(logits, b_labels.view(-1), reduction='sum') / args.batch_size
-
-            loss.backward()
-            optimizer.step()
-
-            train_loss += loss.item()
-            num_batches += 1
-
-        train_loss = train_loss / (num_batches)
-
-        train_acc, train_f1, *_ = model_eval(train_dataloader, model, device)
-        dev_acc, dev_f1, *_ = model_eval(dev_dataloader, model, device)
-
-        if dev_acc > best_dev_acc:
-            best_dev_acc = dev_acc
-            save_model(model, optimizer, args, config, args.filepath)
-
-        print(f"epoch {epoch}: train loss :: {train_loss :.3f}, train acc :: {train_acc :.3f}, dev acc :: {dev_acc :.3f}")
-        '''
+    #Getting val results on Fact Retriever Module(Retriever + Question Classification):
+    acc, f1, y_true, y_preds,avg_loss = retriever_model_eval(val_dataloader, retriever, device)
+    #log the metrics
 
 def test(args):
     with torch.no_grad():
         device = torch.device('cuda') if args.use_gpu else torch.device('cpu')
         saved = torch.load(args.filepath)
         config = saved['model_config']
-        model = BertSentClassifier(config)
+        retriever = RetrieverModel(config)
+        retriever = retriever.to(device)
+        questionClassificationModel = QuestionClassification(config)
+        questionClassificationModel = questionClassificationModel.to(device)
+        #model = BertSentClassifier(config)
         model.load_state_dict(saved['model'])
-        model = model.to(device)
+        #model = model.to(device)
         print(f"load model from {args.filepath}")
-        dev_data = create_data(args.dev, 'valid')
-        dev_dataset = BertDataset(dev_data, args)
-        dev_dataloader = DataLoader(dev_dataset, shuffle=False, batch_size=args.batch_size, collate_fn=dev_dataset.collate_fn)
-
-        test_data = create_data(args.test, 'test')
-        test_dataset = BertDataset(test_data, args)
-        test_dataloader = DataLoader(test_dataset, shuffle=False, batch_size=args.batch_size, collate_fn=test_dataset.collate_fn)
-
-        dev_acc, dev_f1, dev_pred, dev_true, dev_sents = model_eval(dev_dataloader, model, device)
-        test_acc, test_f1, test_pred, test_true, test_sents = model_eval(test_dataloader, model, device)
-
-        with open(args.dev_out, "w+") as f:
-            print(f"dev acc :: {dev_acc :.3f}")
-            for s, t, p in zip(dev_sents, dev_true, dev_pred):
-                f.write(f"{s} ||| {t} ||| {p}\n")
-
+        
+        #get the dataset and dataloaders
+        qc_acc, qc_f1, qc_y_true, qc_y_preds, qc_avg_loss = qc_model_eval(test_dataloader, questionClassificationModel, device)
+        retriever_acc, retriever_f1, retriever_y_true, retriever_y_preds,retriever_avg_loss = retriever_model_eval(test_dataloader, retriever, device)
+        
         with open(args.test_out, "w+") as f:
             print(f"test acc :: {test_acc :.3f}")
-            for s, t, p in zip(test_sents, test_true, test_pred):
-                f.write(f"{s} ||| {t} ||| {p}\n")
+            for t, p in zip( test_true, test_pred):
+                f.write(f"{t} ||| {p}\n")
 
 
 #change the args according to need
