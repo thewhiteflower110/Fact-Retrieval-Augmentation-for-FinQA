002361a5db0c7e98150f831f26414ef0fa21558f
Meet Gandhi <meet.gandhi1415@gmail.com>
2022-10-30 16:24:32 -0400

    Generates prog from logits and facts from logits

---
+++ post_pg.py
@@ -0,0 +1,179 @@
+#This file goes to post-training of Program Generation
+import sys,math
+import collections
+import numpy as np
+all_ops = ["add", "subtract", "multiply", "divide", "exp"]
+
+sys.path.insert(0, '../utils/')
+max_seq_length = 512
+max_program_length = 30
+
+# provides probability of model outputted logits
+def compute_softmax(scores):
+    """Compute softmax probability over raw logits."""
+    if scores == None:
+        return []
+
+    max_score = None
+    for score in scores:
+        if max_score is None or score > max_score:
+            max_score = score
+
+    exp_scores = []
+    total_sum = 0.0
+    for score in scores:
+        x = math.exp(score - max_score)
+        exp_scores.append(x)
+        total_sum += x
+
+    probs = []
+    for score in exp_scores:
+        probs.append(score / total_sum)
+    return probs
+
+#computes program from logits, returns program ids
+def compute_prog_from_logits(logits, max_program_length):
+    pred_prog_ids = []
+    loss = 0
+    for cur_step in range(max_program_length):
+        cur_logits = logits[cur_step]
+        cur_pred_softmax = compute_softmax(cur_logits)
+        #getting the operation with maximum probability
+        #why on logits?
+        #cur_pred_token = np.argmax(cur_logits.cpu()) 
+        #loss -= np.log(cur_pred_softmax[cur_pred_token])
+        cur_pred_token = np.argmax(cur_pred_softmax.cpu()) #get to device 
+        loss -=np.log(cur_pred_softmax[cur_pred_token])
+        pred_prog_ids.append(cur_pred_token)
+        if cur_pred_token == 0:
+            break
+    return pred_prog_ids, loss
+
+#Makes a program from program ids and other stuff
+def indices_to_prog(program_indices, numbers, number_indices,
+                    op_list, op_list_size, const_list, const_list_size):
+    prog = []
+    #iterating over program indices and putting values into a prog list
+    for prog_id in program_indices:
+        if prog_id < op_list_size:
+            prog.append(op_list[prog_id])
+        elif prog_id < op_list_size + const_list_size:
+            prog.append(const_list[prog_id - op_list_size])
+        else:
+            prog.append(numbers[number_indices.index(prog_id - op_list_size
+                                                     - const_list_size)])
+    return prog
+
+#saves the program to the json file for further usage
+def compute_predictions(all_examples, all_features, all_results, n_best_size,
+                        max_program_length, op_list, op_list_size,
+                        const_list, const_list_size):
+    
+    """Computes final predictions based on logits."""
+    example_index_to_features = collections.defaultdict(list)
+    for feature in all_features:
+        example_index_to_features[feature["example_index"]].append(feature)
+
+    unique_id_to_result = {}
+    for result in all_results:
+        unique_id_to_result[result.unique_id] = result
+
+    _PrelimPrediction = collections.namedtuple(  # pylint: disable=invalid-name
+        "PrelimPrediction", [
+            "feature_index", "logits"
+        ])
+
+    all_predictions = collections.OrderedDict()
+    all_predictions["pred_programs"] = collections.OrderedDict()
+    all_predictions["ref_programs"] = collections.OrderedDict()
+    all_nbest = collections.OrderedDict()
+    for (example_index, example) in enumerate(all_examples):
+        if example_index not in example_index_to_features:
+            continue
+        features = example_index_to_features[example_index]
+        prelim_predictions = []
+        for (feature_index, feature) in enumerate(features):
+            if feature["unique_id"] not in unique_id_to_result:
+                continue
+            result = unique_id_to_result[feature["unique_id"]]
+            logits = result.logits
+            prelim_predictions.append(
+                _PrelimPrediction(
+                    feature_index=feature_index,
+                    logits=logits))
+
+        _NbestPrediction = collections.namedtuple(  # pylint: disable=invalid-name
+            "NbestPrediction", "options answer program_ids program")
+
+        nbest = []
+        for pred in prelim_predictions:
+            if len(nbest) >= n_best_size:
+                break
+            program = example.program
+            pred_prog_ids, loss = compute_prog_from_logits(pred.logits,
+                                                           max_program_length)
+            #logger for loss here --
+            pred_prog = indices_to_prog(pred_prog_ids,
+                                        example.numbers,
+                                        example.number_indices,
+                                        op_list, op_list_size,
+                                        const_list, const_list_size
+                                        )
+            nbest.append(
+                _NbestPrediction(
+                    options=example.options,
+                    answer=example.answer,
+                    program_ids=pred_prog_ids,
+                    program=pred_prog))
+
+        # assert len(nbest) >= 1
+        if len(nbest) == 0:
+            continue
+        
+        nbest_json = []
+        for (i, entry) in enumerate(nbest):
+            output = collections.OrderedDict()
+            output["id"] = example.id
+            output["options"] = entry.options
+            output["ref_answer"] = entry.answer
+            output["pred_prog"] = [str(prog) for prog in entry.program]
+            output["ref_prog"] = example.program
+            output["question_tokens"] = example.question_tokens
+            output["numbers"] = example.numbers
+            output["number_indices"] = example.number_indices
+            nbest_json.append(output)
+
+        assert len(nbest_json) >= 1
+
+        all_predictions["pred_programs"][example_index] = nbest_json[0]["pred_prog"]
+        all_predictions["ref_programs"][example_index] = nbest_json[0]["ref_prog"]
+        all_nbest[example_index] = nbest_json
+
+    return all_predictions, all_nbest
+
+
+'''
+Add this to pg_eval
+all_results = []
+
+        
+        for output_dict in self.predictions:
+            all_results.append(
+                RawResult(
+                    unique_id=output_dict["unique_id"],
+                    logits=output_dict["logits"],
+                    loss=None
+                ))
+            
+        all_predictions, all_nbest = compute_predictions(
+            data_ori,
+            data,
+            all_results,
+            n_best_size=self.n_best_size,
+            max_program_length=self.program_length,
+            #tokenizer=self.tokenizer,
+            op_list=op_list,
+            op_list_size=len(op_list),
+            const_list=const_list,
+            const_list_size=len(const_list))
+'''
---
+++ post_retriever.py
@@ -0,0 +1,104 @@
+import json
+#This file goes to post-training of retriever
+'''
+1. Evaluates the output of the model according to facts extracted
+2. Adds the retrieved facts to the data files for PG Module to work on it.
+'''
+def retriever_evaluate(output_dict, ori_file, topn):
+    '''
+    save results to file. calculate recall
+    batch_logits = logits for the given indices(each row=[_,score], columns=#text evidences)
+    batch_filename_ids = file uids (uid used)
+    batch_inds = evidence ids [indices]
+    ori_file = original file
+    topn = #number of top facts to be extracted
+    '''
+    #Sample example of 1 pass of for loop [0.4,0.5,0.1] , uid=45, facts=[4,90,24]
+    all_files_logits = output_dict["logits"]
+    all_files_filename_ids = output_dict["filename_id"]
+    all_files_evidence_inds = output_dict["ind"]
+    
+    res_filename = {}
+    res_filename_inds = {}
+    #Understand the format of logits!
+    for this_logit, this_filename_id, this_ind in zip(all_files_logits, all_files_filename_ids, all_files_evidence_inds):
+        
+        #declaring lists for every uid
+        if this_filename_id not in res_filename:
+            res_filename[this_filename_id] = []
+            res_filename_inds[this_filename_id] = []
+            
+        #assigning scores for every batch_index
+        if this_ind not in res_filename_inds[this_filename_id]:
+            res_filename[this_filename_id].append({
+                "score": this_logit[1].item(), #why 1 here?
+                "ind": this_ind
+            })
+            res_filename_inds[this_filename_id].append(this_ind)
+
+    #get all entries from train file 
+    with open(ori_file) as f:
+        data_all = json.load(f)
+    
+    #all_files_recall = 0.0
+    
+    #Go by each entry, and check if res_filename contains that id
+    count_data = 0
+    for data in data_all:
+        this_filename_id = data["uid"]
+        
+        #Discarding files that are not in our results
+        if this_filename_id not in res_filename:
+            continue
+        
+        #since it is present in true fact, we add 1
+        count_data += 1
+        
+        #get all the text evidence indices of this file
+        this_res = res_filename[this_filename_id]
+        
+        #sorting them according to its score, with highest score first
+        sorted_dict = sorted(this_res, key=lambda kv: kv["score"], reverse=True)
+        
+        # sorted_dict = sorted_dict[:topn]
+        
+        #true evidence ids
+        true_sent_inds = data["qa"]["text_evidence"]
+        true_table_inds = data["qa"]["table_evidence"]
+
+        # table rows
+        pred_table_inds_topn = []
+        pred_sent_inds_topn = []
+        # all retrieved
+        pred_table_inds = []
+        pred_sent_inds = []
+        
+        for tmp in sorted_dict[:topn]:
+            if type(tmp["ind"]) == str:
+                pred_table_inds_topn.append(tmp)
+            else:
+                pred_sent_inds_topn.append(tmp)
+
+        for tmp in sorted_dict:
+            if type(tmp["ind"]) == str:
+                pred_table_inds.append(tmp)
+            else:
+                pred_sent_inds.append(tmp)
+
+        #correct_table_ids = len(set(pred_table_inds_topn).intersection(true_table_inds)) 
+        #correct_text_ids = len(set(pred_sent_inds_topn).intersection(true_sent_inds)) 
+        
+        #all_files_recall += (correct_table_ids+correct_text_ids) / (len(true_table_inds) + len(true_sent_inds))
+
+        #putting all the predictions in the file again,
+        data["table_retrieved_all"] = pred_table_inds
+        data["text_retrieved_all"] = pred_sent_inds
+        data["table_retrieved_topn"] = pred_table_inds_topn
+        data["text_retrieved_topn"] = pred_sent_inds_topn
+
+    #res = all_files_recall / len(data_all)
+    
+    #res_message = f"Top {topn}: {res}\n"
+    
+    #return res, res_message
+    return None
