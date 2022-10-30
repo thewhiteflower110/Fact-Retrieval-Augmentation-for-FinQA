9fac5caa1e9e90d5a7df87cbd4067a1c699b5ab7
Juhi Kamdar <43910571+thewhiteflower110@users.noreply.github.com>
2022-10-30 18:06:13 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ pwd Fact-Retrieval-Augmentation-for-FinQA/
 python3 setup.py install
 #Accessing things with smaller names is easier #Note: change Later!
 mv ./Fact-Retrieval-Augmentation-for-FinQA-main ./fa
+#Getting the dataset and moving to data folder
 # Finetuning Fact Retrieval Module with table-> text data
 python3 train_fr_module.py 
 #Converting output of Fact Retrieval Module to input of Reasoning Module
